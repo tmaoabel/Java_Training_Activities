de41e6686b8c78ccfe44e74f7670d0ae3d777ce1
Trisha Mae A. Oabel <tmaoabel@bpi.com.ph>
2025-12-05 11:17:42 +0800

    M2_Activity2

---
+++ Oabel_Seatwork2.java
@@ -0,0 +1,12 @@
+package OOP_Seatwork2;
+
+public class Oabel_Seatwork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Whiteboard wb = new Whiteboard();
+        int sum = wb.addNumbers(5,3);
+        System.out.println("The total is: " + String.valueOf(sum) );
+	}
+
+}
